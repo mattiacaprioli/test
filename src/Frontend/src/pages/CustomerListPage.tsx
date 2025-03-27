3248c97131ebe652c9f888eb2dfe3b4dc19cc8cf
Mattia Caprioli <119086682+mattiacaprioli@users.noreply.github.com>
2025-03-27 10:52:34 +0000

    test: check frontend-backend connection

--- src/Frontend/src/pages/CustomerListPage.tsx
+++ src/Frontend/src/pages/CustomerListPage.tsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from 'react';
+
+type Customer = {
+  id: number;
+  name: string;
+  address: string;
+  email: string;
+  phone: string;
+  iban: string;
+  category?: {
+    code: string;
+    description: string;
+  };
+};
+
 export default function CustomerListPage() {
-    return <>
-        <i>Todo customers</i>
+  const [customers, setCustomers] = useState<Customer[]>([]);
+
+  useEffect(() => {
+    fetch('/api/customers/list')
+      .then(async res => {
+        console.log('Response status:', res.status);
+        if (!res.ok) throw new Error(`Errore HTTP: ${res.status}`);
+        return res.json();
+      })
+      .then(setCustomers)
+      .catch(err => console.error('Errore fetch customers', err));
+  }, []);
+
+  console.log('cursomers: ', customers)
+
+  return (
+    <>
+      <h2>Customer List</h2>
+      {customers.length === 0 ? (
+        <i>Nessun cliente trovato</i>
+      ) : (
+        <ul>
+          {customers.map(c => (
+            <li key={c.id}>
+              <b>{c.name}</b> - {c.email} [{c.category?.code}]
+            </li>
+          ))}
+        </ul>
+      )}
     </>
+  );
 }
